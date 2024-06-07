93219007fb610b8c2cbda83c537939274ce1fca6
huyle <huyle210504@gmail.com>
2024-06-07 16:18:28 +0700

    modified repo

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,5 +7,20 @@ import joblib
 
 # dashboard
 import streamlit as st
+import altair as alt
+import plotly.express as px
 
-st.title('Customer Segmentation of Online Retail')
+# load dataset
+df_base = pd.read_csv('../dataset/rfm.csv')
+
+
+st.set_page_config(
+    page_title='Online Retail', 
+    layout='wide'
+)
+alt.themes.enable('dark')
+
+with st.sidebar:
+    st.title('Online Retail')
+
+    selected_analysis_type = st.selectbox('Select a type of analysis', ['Exploratory Data Analysis', 'Customer Segmentation'])
