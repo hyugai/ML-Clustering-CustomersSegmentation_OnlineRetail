0b62172000de785feff9189007b7b5bf144bf1d8
huyle <huyle210504@gmail.com>
2024-06-07 15:37:19 +0700

    modified repo

---
+++ dashboard/app.py
@@ -0,0 +1,11 @@
+# data structures
+import numpy as np
+import pandas as pd
+
+# load model
+import joblib
+
+# dashboard
+import streamlit as st
+
+st.title('Customer Segmentation of Online Retail')
