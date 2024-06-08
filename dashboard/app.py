773671e03ba096d34f227f731db789d8797af523
huyle <huyle210504@gmail.com>
2024-06-08 22:39:47 +0700

    modified repo

--- dashboard/app.py
+++ dashboard/app.py
@@ -14,9 +14,6 @@ import streamlit as st
 import altair as alt
 import plotly.express as px
 
-# others
-import datetime
-
 # load dataset
 df_base = pd.read_csv('../dataset/labeled_rfm.csv')
 df_base['label'] = df_base['label'].astype(str)
@@ -24,6 +21,8 @@ df_base['label'] = df_base['label'].astype(str)
 new_label = {'0': 'Whales', '2': 'New customers (2nd)', '1': 'One-time buyers', '3': 'New cutomers (1st)'}
 df_base['Segmentation\'s type'] = df_base['label'].map(new_label)
 
+size = ((df_base['Segmentation\'s type'].value_counts() / len(df_base))*100).to_dict()
+size = np.round(df_base['Segmentation\'s type'].map(size), 0)
 
 # load models
 scaler = joblib.load('../models/scaler.pkl')
@@ -38,14 +37,15 @@ alt.themes.enable('dark')
 with st.sidebar:
     st.title('Online Retail')
 
-    seleted_mode = st.selectbox('Select a type of analysis', ['Exploratory Data Analysis', 'Customer Segmentation'])
+    seleted_mode = st.selectbox('Select a type of analysis', 
+                                ['Exploratory Data Analysis', 'Customer Segmentation'])
 
 if seleted_mode == 'Exploratory Data Analysis':
     st.write('hello world')
 
 # segmentation
 elif seleted_mode == 'Customer Segmentation':
-    col1, col2 = st.columns([0.1, 0.3])
+    col1, col2 = st.columns([0.2, 0.8])
 
     with col1:
         ###
@@ -70,19 +70,32 @@ elif seleted_mode == 'Customer Segmentation':
             format='%f'
         )
 
-    fig = px.scatter_3d(data_frame=df_base, x='recency', y='frequency', z='monetary', 
-                        color='Segmentation\'s type', opacity=0.55,
-                        color_discrete_sequence=[
-                            px.colors.qualitative.Pastel1[0],
-                            px.colors.qualitative.Pastel[1],
-                            px.colors.qualitative.Pastel1[2],
-                            px.colors.qualitative.Pastel1[3]
-                        ], 
-                        width=1000, height=800)
-    fig.update_layout(title='The \'\'Recency-Frequency-Monetary\'\' space')
-    fig.add_trace(go.Scatter3d(x=[50], y=[100], z=[500], name='Customer',
-                            mode='markers+text', 
-                            text='<b>Your customer here!</b>', textfont=dict(color=px.colors.qualitative.Light24[0], size=20, style='italic'),
-                            marker=go.scatter3d.Marker(color=px.colors.qualitative.D3[3], size=10.5, symbol='diamond')))
+        ###
 
-    st.plotly_chart(fig)
+    ##
+    with col2:
+        fig = px.scatter_3d(data_frame=df_base, x='recency', y='frequency', z='monetary', 
+                            color='Segmentation\'s type', opacity=0.5, size=size,
+                            color_discrete_sequence=[
+                                px.colors.qualitative.Dark2[0],
+                                px.colors.qualitative.Dark2[1],
+                                px.colors.qualitative.Dark2[2],
+                                px.colors.qualitative.Dark2[3]
+                            ], 
+                            width=1200, height=700
+                            )
+        fig.update_traces(marker=dict(line=dict(width=0)))
+
+        fig.add_trace(go.Scatter3d(x=[50], y=[100], z=[500], name='Customer',
+                                mode='markers+text', 
+                                text='<b>Your customer\'s here!</b>', textfont=dict(color=px.colors.qualitative.Plotly[5], size=20, style='italic'),
+                                marker=go.scatter3d.Marker(color=px.colors.qualitative.Plotly[5], size=7, symbol='diamond')))
+        
+        fig.update_layout(
+            title=dict(
+                text='The \'\'Recency-Frequency-Monetary\'\' space',
+                x=0.4, y=0.9,
+                xanchor='center', yanchor='top'
+                ))
+
+        st.plotly_chart(fig)
