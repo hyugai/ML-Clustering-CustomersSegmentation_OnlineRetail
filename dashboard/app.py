ec05215f4f52454ddb7e361e85438ab29d0df420
huyle <huyle210504@gmail.com>
2024-06-08 10:53:16 +0700

    modified repo

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,4 +1,5 @@
 # data structures
+# %%
 import numpy as np
 import pandas as pd
 
@@ -14,14 +15,20 @@ import streamlit as st
 import altair as alt
 import plotly.express as px
 
+# others
+import datetime
+
 # load dataset
 df_base = pd.read_csv('../dataset/labeled_rfm.csv')
 df_base['label'] = df_base['label'].astype(str)
 
+new_label = {'0': 'Whales', '2': 'New customers (2nd)', '1': 'One-time buyers', '3': 'New cutomers (1st)'}
+df_base['Segmentation\'s type'] = df_base['label'].map(new_label)
+
+
 # load models
 scaler = joblib.load('../models/scaler.pkl')
 kmeans = joblib.load('../models/4Clusters_model.pkl')
-
 
 st.set_page_config(
     page_title='Online Retail', 
@@ -44,7 +51,8 @@ elif seleted_mode == 'Customer Segmentation':
     with col1:
         ###
         latest_date = st.date_input(
-            label='The date since the last purchase'
+            label='The date since the last purchase', 
+            min_value=pd.to_datetime('2010-12-01')
         )
         min_date = pd.to_datetime('2010-12-01')
         recency = (pd.to_datetime(latest_date) - min_date).days
@@ -68,14 +76,17 @@ elif seleted_mode == 'Customer Segmentation':
             st.write('Testing')
 
     fig = px.scatter_3d(data_frame=df_base, x='recency', y='frequency', z='monetary', 
-                        color='label',
+                        color='Segmentation\'s type',
                         color_discrete_sequence=[
                             px.colors.qualitative.Pastel1[0],
                             px.colors.qualitative.Pastel[1],
                             px.colors.qualitative.Pastel1[2],
                             px.colors.qualitative.Pastel1[3]
                         ], 
-                        opacity=0.5)
-    fig.add_trace(go.Scatter3d(x=[50], y=[100], z=[500], name='Your customer',
-                               mode='markers', marker=go.scatter3d.Marker(color=px.colors.qualitative.D3[3], size=10.5, symbol='diamond')))
+                        opacity=0.55)
+    fig.add_trace(go.Scatter3d(x=[50], y=[100], z=[500], name='Customer',
+                               mode='markers+text', 
+                               text='<b>Your customer here!</b>', textfont=dict(color=px.colors.qualitative.Light24[0], size=20, style='italic'),
+                               marker=go.scatter3d.Marker(color=px.colors.qualitative.D3[3], size=10.5, symbol='diamond')))
+   
     st.plotly_chart(fig, theme=None)
