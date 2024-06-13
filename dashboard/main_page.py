500eec8e17b36feb9c23bcbfd1d53ce7cf619566
huyle <huyle210504@gmail.com>
2024-06-13 15:36:56 +0700

    modified repo

--- dashboard/main_page.py
+++ dashboard/main_page.py
@@ -23,3 +23,11 @@ kmeans = joblib.load('../models/kmeans.pkl')
 st.set_page_config(page_title='Customer Segmentation: Online Retail', 
                    layout='wide')
 st.title('Overviews')
+st.text(
+    body='The power of data has gradually accepted for recent years in both traditional ways and modern ones.\
+        \nBeside charts, we can now use machine learning to uncover hidden patters and help businesses unleash their potential.\
+        \nThis dashboard will guide you through informations that machine learning model (KMeans) can find with 2 pages:'
+)
+st.markdown('##### 1. Exploratory Data Analysis\
+            \n##### 2. Visualize Segmentation\
+            \n#### Enjoys!')
