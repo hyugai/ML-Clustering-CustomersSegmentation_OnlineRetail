94e3ccd81c1b5e4c71b7d50d6cbcd88919b48886
huyle <huyle210504@gmail.com>
2024-06-13 15:59:51 +0700

    modified repo

--- dashboard/main_page.py
+++ dashboard/main_page.py
@@ -1,23 +1,5 @@
-# data structures
-import numpy as np
-import pandas as pd
-
-# visualization
-import plotly.express as px
-import plotly.graph_objects as po
-
 # dashboard
 import streamlit as st
-
-# others
-import joblib
-
-## load dataset
-df_base = pd.read_csv('../../dataset/labeled_rfm.csv')
-
-## load model
-scaler = joblib.load('../../models/scaler.pkl')
-kmeans = joblib.load('../../models/kmeans.pkl')
 
 ###
 st.set_page_config(page_title='Customer Segmentation: Online Retail', 
